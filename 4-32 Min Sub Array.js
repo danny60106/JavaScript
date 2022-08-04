29b696a3e0873cb17a34681f18e9df61c446c7bd
danny60106 <54652877+danny60106@users.noreply.github.com>
2022-08-04 20:26:15 +0800

    Update 4-32 Min Sub Array.js

--- 4-32 Min Sub Array.js
+++ 4-32 Min Sub Array.js
@@ -1,3 +1,15 @@
+/*
+    Write a function called minSubLength which accepts two parameters, an array of
+    positive integers and a positive integer.This function should return the minimal
+    length of a continuous subarray - the sum of elements inside this subarray has to
+    be greater than or equal to the positive integer parameter.If subarray not found,
+    then return 0.
+    
+    Ex.
+    minSubLength([9, 8, 1, 4, 9, 5, 1, 2], 11)); //2
+*/
+
+
 maxSum([2, 7, 3, 7, 25, 0, 6, 1, -5, 12, -11], 3); // 12
 
 function maxSum(arr, size) {
