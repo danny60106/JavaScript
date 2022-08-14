50addb898aa5e6d293266665dad10183ee0f047a
danny60106 <54652877+danny60106@users.noreply.github.com>
2022-08-14 21:43:17 +0800

    Update 4-37 Array of Arrays.js

--- 4-37 Array of Arrays.js
+++ 4-37 Array of Arrays.js
@@ -5,6 +5,23 @@
 
 let arrs = [[[["a", [["b", ["c"]], ["d"]]], [["e"]], [[["f", "g", "h"]]]]]];
 
+#1
+let result = [];
+
+function collector(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            collector(arr[i]);
+        } else{
+            result.push(arr[i]);
+        }
+    }
+}
+
+collector(arrs);
+console.log(result);
+
+#2
 function collector(arr1) {
   let result = [];
   helper(arr1);
